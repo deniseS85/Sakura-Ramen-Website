d4f4f25f057a8d2950a360763ac39e496d2cacc7
Denise <122867929+deniseS85@users.noreply.github.com>
2023-01-28 11:45:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function translateGerman() {
+    document.getElementById('menu').innerHTML = 'Menü';
+    document.getElementById('location').innerHTML = 'Standort';
+    document.getElementById('about').innerHTML = 'Über Uns';
+    document.getElementById('underHeadline').innerHTML = 'Der beste Ramen in der Stadt';
+    document.getElementById('order').innerHTML = 'Wie können Sie bestellen';
+    document.getElementById('order1').innerHTML = 'Wählen Sie<br>Ihre Nudeln';
+    document.getElementById('order2').innerHTML = 'Wählen Sie<br>Ihre Sauce';
+    document.getElementById('order3').innerHTML = 'Fügen Sie<br>Ihre Beilage hinzu';
+    document.getElementById('ourMenu').innerHTML = 'Unser Menü';
+    document.getElementById('chicken1').innerHTML = 'Huhn';
+    document.getElementById('beef1').innerHTML = 'Rind';
+    document.getElementById('shrimp1').innerHTML = 'Krabben';
+    document.getElementById('chicken2').innerHTML = 'Huhn';
+    document.getElementById('beef2').innerHTML = 'Rind';
+    document.getElementById('shrimp2').innerHTML = 'Krabben';
+    document.getElementById('chicken3').innerHTML = 'Huhn';
+    document.getElementById('beef3').innerHTML = 'Rind';
+    document.getElementById('shrimp3').innerHTML = 'Krabben';
+    document.getElementById('findUs').innerHTML = 'Sie finden uns hier';
+}
+
+function translateEnglish() {
+    document.getElementById('menu').innerHTML = 'Menu';
+    document.getElementById('location').innerHTML = 'Location';
+    document.getElementById('about').innerHTML = 'About';
+    document.getElementById('underHeadline').innerHTML = 'The best Ramen in town';
+    document.getElementById('order').innerHTML = 'How to order';
+    document.getElementById('order1').innerHTML = 'Pick<br>Your Noodles';
+    document.getElementById('order2').innerHTML = 'Pick<br>Your Broth';
+    document.getElementById('order3').innerHTML = 'Add Your<br>Toppings';
+    document.getElementById('ourMenu').innerHTML = 'Our Menu';
+    document.getElementById('chicken1').innerHTML = 'Chicken';
+    document.getElementById('beef1').innerHTML = 'Beef';
+    document.getElementById('shrimp1').innerHTML = 'Shrimp';
+    document.getElementById('chicken2').innerHTML = 'Chicken';
+    document.getElementById('beef2').innerHTML = 'Beef';
+    document.getElementById('shrimp2').innerHTML = 'Shrimp';
+    document.getElementById('chicken3').innerHTML = 'Chicken';
+    document.getElementById('beef3').innerHTML = 'Beef';
+    document.getElementById('shrimp3').innerHTML = 'Shrimp';
+    document.getElementById('findUs').innerHTML = 'Find us at';
+}
